685d11204fb369b0d0eeb44dad21fa3219605e22
= <kaven.feng@autodesk.com>
2015-05-10 14:20:03 +0800

    create task

--- www/js/app.js
+++ www/js/app.js
@@ -18,10 +18,34 @@ angular.module('starter', ['ionic'])
   });
 
 })
-.controller('TodoCtrl', function($scope){
-    $scope.Tasks = [
-      { title : 'aaa'},
-      { title : 'bbb'},
-      { title : 'ccc'}
-    ];
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 });
